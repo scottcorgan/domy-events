eb4a98e4a3d289a34054ef937e045743b1ac5bdf
Scott Corgan <scottcorgan@gmail.com>
2013-12-21 15:21:44 -0800

    Add initial logic

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-ear
-===
-
+# ear
+ 
 Map DOM events to DOM elements
+ 
+## Install
+ 
+```
+npm install ear --save
+```
+ 
+## Usage
+ 
+```js
+// Usage
+```
+ 
+## Run Tests
+ 
+```
+npm install
+npm test
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+(function () {
+  var EVENT_SPLITTER = /\s+/;
+  var windowLoaded = false;
+
+  var ear = function (events, options) {
+    if (windowLoaded) init();
+    else window.onload = init;
+    
+    function init() {
+      windowLoaded = true;
+      Object.keys(events).forEach(ear.mapEvents(events, options));
+    }
+    
+    return ear;
+  };
+
+  ear.mapEvents = function (events, options) {
+    return function (key) {
+      if (!EVENT_SPLITTER.test(key)) return;
+      
+      var i = 0;
+      var eventList = key.split(EVENT_SPLITTER);
+      var eventName = eventList.shift();
+      var action = events[key];
+      var parent = ear.parentElement(options.parent);
+      
+      if (!parent) return;
+      
+      var domElements = parent.querySelectorAll(eventList.join(' '));
+      
+      for(i; i < domElements.length; i += 1) {
+        var el = domElements[i];
+        if (!el) next;
+        
+        el.addEventListener(eventName, function (e) {
+          var context = options.bind || this;
+          action.call(context, e);
+        });
+      }
+    };
+  };
+
+  ear.parentElement = function (parent) {
+    if (!parent) return document;
+    if (typeof parent === 'object') return parent;
+    
+    return document.querySelector(parent);
+  }
+
+  module.exports = ear;
+}());
