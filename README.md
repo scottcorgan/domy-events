61900f3efbc78b9e5ea762ef3ace05a61d03e010
Scott Corgan <scottcorgan@gmail.com>
2013-12-24 10:06:51 -0800

    Rename and add delegation

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# ear
+# dove-events
  
 Map DOM events to DOM elements. For use with [Browserify](http://browserify.org/).
  
 ## Install
  
 ```
-npm install ear --save
+npm install dove-events --save
 ```
  
 ## Usage
  
 ```js
-var ear = require('ear');
+var events = require('dove-events');
 
 var obj = {
   method: function (e) {
@@ -20,20 +20,26 @@ var obj = {
   }
 };
 
-ear({
-  'click .some-css-selector': obj.method
-}, {
+var eventsOptions = {
   bind: obj, // optional
-  parent: '.parent' // optional
-});
+  parent: '.parent', // optional (can use a DOM object as well)
+  delegate: true // optional
+};
+
+events({
+  'click .some-css-selector': obj.method
+}, eventsOptions);
 ```
  
-Note: **ear** uses `querySelectorAll` under the hood.
+If you bind the events to a context/object, you can provide a string ans the event listener method instead of a reference to the method.
+ 
+Note: **dove-events** uses `querySelectorAll` under the hood.
 
 ## Options
 
 * `bind` - the context in which to bind the event handler method to
-* `parent` - the parent DOM element for all css selections. Defaults to `document`
+* `parent` - the parent DOM element for all css selections. Defaults to `document`.
+* `delegate` - delegate all events to the parent
  
 ## Browser support
 
--- index.js
+++ index.js
@@ -1,50 +1,71 @@
-(function () {
-  var EVENT_SPLITTER = /\s+/;
-  var windowLoaded = false;
+var EVENT_SPLITTER = /\s+/;
+var windowLoaded = false;
 
-  var ear = function (events, options) {
-    if (windowLoaded || document.readyState === "complete") init();
-    else window.onload = init;
+var doveEvents = function (events, options) {
+  if (windowLoaded || document.readyState === "complete") init();
+  else window.onload = init;
+  
+  function init() {
+    setTimeout(function () {
+      windowLoaded = true;
+      Object.keys(events).forEach(doveEvents._mapEvents(events, options));
+    }, 0);
+  }
+  
+  return doveEvents;
+};
+
+doveEvents._mapEvents = function (events, options) {
+  options || (options = {});
     
-    function init() {
-      setTimeout(function () {
-        windowLoaded = true;
-        Object.keys(events).forEach(ear.mapEvents(events, options));
-      }, 0);
+  return function (key) {
+    // Valid event map?
+    if (!EVENT_SPLITTER.test(key)) return;
+    
+    var eventList = key.split(EVENT_SPLITTER);
+    var eventName = eventList.shift();
+    var action = events[key];
+    var parent = doveEvents._parentElement(options.parent);
+    var domElements = parent.querySelectorAll(eventList.join(' '));
+    
+    if (options.delegate) {
+      parent.addEventListener(eventName, function (e) {
+        if ([].slice.call(domElements, 0).indexOf(e.target) < 0) return;
+        doveEvents._callAction(e, action, options);
+      });
     }
-    
-    return ear;
-  };
-
-  ear.mapEvents = function (events, options) {
-    return function (key) {
-      if (!EVENT_SPLITTER.test(key)) return;
-      
-      var eventList = key.split(EVENT_SPLITTER);
-      var eventName = eventList.shift();
-      var action = events[key];
-      var parent = ear.parentElement(options.parent);
-      
-      if (!parent) return;
-      
-      var domElements = parent.querySelectorAll(eventList.join(' '));
-      
+    else {
       [].forEach.call(domElements, function (el) {
         el.addEventListener(eventName, function (e) {
-          var context = options.bind || this;
-          action.call(context, e);
+          doveEvents._callAction(e, action, options);
         });
       });
-      
-    };
-  };
-
-  ear.parentElement = function (parent) {
-    if (!parent) return document;
-    if (typeof parent === 'object') return parent;
+    }
     
-    return document.querySelector(parent);
-  }
+  };
+};
 
-  module.exports = ear;
-}());
+doveEvents._callAction = function (e, action, options) {
+  var context = options.bind || this;
+  doveEvents._parseAction(action, context).call(context, e);
+};
+
+doveEvents._parseAction = function (action, context) {
+  if (typeof action === 'string') {
+    var __action = action;
+    
+    action = context[action];
+    if (!action) throw new Error('Event listener method "' + __action + '" is undefined');
+  }
+  
+  return action;
+};
+
+doveEvents._parentElement = function (parent) {
+  if (!parent) return document;
+  if (typeof parent === 'object') return parent;
+  
+  return document.querySelector(parent);
+}
+
+module.exports = doveEvents;
